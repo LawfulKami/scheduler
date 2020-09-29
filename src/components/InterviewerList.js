ab518953708e7fe38bb95e4b739456bfcb5f5a29
Lanny Rivest <lanny.rivest@gmail.com>
2020-09-29 15:06:20 -0400

    List done, set up for appointment

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+
+const items = props.interviewers.map((interviewer) => {
+  const { name, avatar, id } = interviewer
+  return <InterviewerListItem 
+  key={id}
+  name={name}
+  avatar={avatar}
+  selected= {props.value === interviewer.id}
+  setInterviewer={props.onChange}
+  />
+})
+
+
+  return (
+<section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+<ul className="interviewers__list">{items}</ul>
+</section>
+  );
+}
+
+
+
+
